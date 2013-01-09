acdb750e7fc559d7aeb05d139d19c04f2708c900
Pedro Melo <melo@simplicidade.org>
2013-01-09 22:49:16 +0000

    Cleanup Vimes.pm, add abstract and a bit of history
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Vimes.pm
+++ lib/Vimes.pm
@@ -1,11 +1,25 @@
 package Vimes;
 
-# ABSTRACT: a very cool module
+# ABSTRACT: He who watches the watchmen
+# VERSION
+# AUTHORITY
 
 use strict;
 use warnings;
 
-# VERSION
-# AUTHORITY
-
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 HISTORY
+
+The name for this module, Vimes, is a tribute to one of my favorite
+authors, Terry Pratchett, and his book series, Discworld. It's named
+after one of my favorite characters, Sam Vimes, the commander of the
+City Watch, whose job is to try to maintain order in a chaotic city.
+
+Just like a good process supervisor should...
+
+=cut
